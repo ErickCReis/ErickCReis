86457981b0b0d6c9e7ca30837b8083f1d0f3856e
Erick Reis <erickcorreareis@gmail.com>
2026-02-27 22:19:41 -0300

    Enhance cursor presence layer with tweening effects and optimize cursor position publishing throttle

--- src/hooks/use-cursor-presence.ts
+++ src/hooks/use-cursor-presence.ts
@@ -32,7 +32,7 @@ export function useCursorPresence() {
     publishCursor(payload);
   };
 
-  const throttledPublishCursorPosition = throttle(publishCursorPosition, 80);
+  const throttledPublishCursorPosition = throttle(publishCursorPosition, 200);
 
   createEffect(() => {
     const x = mouse.x;
